f4aa2586a7db3a3407087bb4aa2501892b8ae24a
DeShon Dixon <deshondixon@gmail.com>
2023-06-26 14:43:18 -0700

    acp

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -7,8 +7,22 @@ import Button from '@mui/material/Button';
 
 export default function Employee() {
   const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' };
-  const [name, setName] = useState(' ');
-  const [password, setPassword] = useState(' ');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const handleClick = (e) => {
+    e.preventDefault();
+    const employee = { firstName, lastName, username, password };
+    console.log(employee);
+    fetch('http://localhost:8080/auth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(employee),
+    }).then((response) => {
+      console.log('employee registered');
+    });
+  };
 
   return (
     <Container>
@@ -26,24 +40,43 @@ export default function Employee() {
         >
           <TextField
             id='outlined-basic'
-            label='Employee Name'
+            label='First Name'
             variant='outlined'
             fullWidth
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
           />
           <TextField
             id='outlined-basic'
-            label='Employee Password'
+            label='Last Name'
+            variant='outlined'
+            fullWidth
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <TextField
+            id='outlined-basic'
+            label='Username'
+            variant='outlined'
+            fullWidth
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            id='outlined-basic'
+            label='Password'
             variant='outlined'
             fullWidth
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <Button variant='contained'>Register</Button>
+          <Button variant='contained' onClick={handleClick}>
+            Register
+          </Button>
         </Box>
-        {name}
-        {password}
+        {firstName}
+        {lastName}
+        {username}
       </Paper>
     </Container>
   );
