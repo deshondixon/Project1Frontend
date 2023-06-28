f24861f7c4b71a44feffe3c3198098da2b788b8a
DeShon Dixon <deshondixon@gmail.com>
2023-06-28 13:10:29 -0700

    acp

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,9 +19,29 @@ export default function Login() {
       body: JSON.stringify(credentials),
     })
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data);
+        console.log(data.accessToken);
+        console.log(parseJwt(data.accessToken));
+      })
       .catch((error) => console.log(error));
   };
+
+  function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(
+      window
+        .atob(base64)
+        .split('')
+        .map(function (c) {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join('')
+    );
+
+    return JSON.parse(jsonPayload);
+  }
 
   return (
     <Container>
