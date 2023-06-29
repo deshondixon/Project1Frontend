48338a429b4e8011cdb998df8c7853a4558ae483
DeShon Dixon <deshondixon@gmail.com>
2023-06-29 14:15:31 -0700

    acp

--- src/components/Reimbursement.js
+++ src/components/Reimbursement.js
@@ -1,107 +1,8 @@
-// import React, { useState, useEffect } from 'react';
-// import Container from '@mui/material/Container';
-// import Paper from '@mui/material/Paper';
-// import Button from '@mui/material/Button';
-// import TextField from '@mui/material/TextField';
-
-// export default function Reimbursement() {
-//   const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' };
-//   const [id, setId] = useState('');
-//   const [description, setDescription] = useState('');
-//   const [status, setStatus] = useState('');
-//   const [expenseAmount, setExpenseAmount] = useState('');
-//   const [reimbursements, setReimbursements] = useState([]);
-//   const [submissionStatus, setSubmissionStatus] = useState('');
-
-//   const handleClick = (e) => {
-//     e.preventDefault();
-//     const reimbursement = { id, description, status, expenseAmount };
-//     console.log(reimbursement);
-//     fetch('http://localhost:8080/reimbursements', {
-//       method: 'POST',
-//       headers: { 'Content-Type': 'application/json' },
-//       body: JSON.stringify(reimbursement),
-//     })
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(data);
-//         setReimbursements([...reimbursements, data]);
-//         setSubmissionStatus('Successfully submitted!');
-//       })
-//       .catch((error) => console.log(error));
-//   };
-
-//   useEffect(() => {
-//     fetch('http://localhost:8080/reimbursements')
-//       .then((res) => res.json())
-//       .then((data) => setReimbursements(data))
-//       .catch((error) => console.log(error));
-//   }, []);
-
-//   return (
-//     <Container>
-//       <Paper elevation={20} style={paperStyle}>
-//         <div>
-//           <h2>Submit A Ticket</h2>
-//           <form>
-//             <TextField
-//               label='ID'
-//               placeholder='Enter ID'
-//               value={id}
-//               onChange={(e) => setId(e.target.value)}
-//               fullWidth
-//               required
-//             />
-//             <TextField
-//               label='Description'
-//               placeholder='Enter Description'
-//               value={description}
-//               onChange={(e) => setDescription(e.target.value)}
-//               fullWidth
-//               required
-//             />
-//             <TextField
-//               label='Status'
-//               placeholder='Enter status'
-//               value={status}
-//               onChange={(e) => setStatus(e.target.value)}
-//               fullWidth
-//               required
-//             />
-//             <TextField
-//               label='Expense Amount'
-//               placeholder='Enter Expense Amount'
-//               type='password'
-//               value={expenseAmount}
-//               onChange={(e) => setExpenseAmount(e.target.value)}
-//               fullWidth
-//               required
-//             />
-//             <Button
-//               type='submit'
-//               variant='contained'
-//               color='primary'
-//               onClick={handleClick}
-//               fullWidth
-//             >
-//               Register
-//             </Button>
-//           </form>
-//           {submissionStatus && <p>{submissionStatus}</p>}
-//         </div>
-//       </Paper>
-//     </Container>
-//   );
-// }
-
 import React, { useState } from 'react';
-import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import { Input, Spacer, Text, Container, Grid, Card } from '@nextui-org/react';
+import { Button } from '@mui/material';
 
 export default function Reimbursement() {
-  const paperStyle = { padding: '50px 20px', width: 600, margin: '20px auto' };
   const [description, setDescription] = useState('');
   const [expenseAmount, setExpenseAmount] = useState('');
   const [submissionStatus, setSubmissionStatus] = useState('');
@@ -131,50 +32,112 @@ export default function Reimbursement() {
   };
 
   return (
-    <Container>
-      <Paper elevation={20} style={paperStyle}>
-        <div>
-          <h2>Submit A Ticket</h2>
-          <form>
-            <TextField
-              label='Description'
-              placeholder='Enter Description'
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              fullWidth
-              required
-            />
-            <TextField
-              label='Expense Amount'
-              placeholder='Enter Expense Amount'
-              type='number'
-              value={expenseAmount}
-              onChange={(e) => setExpenseAmount(e.target.value)}
-              fullWidth
-              required
-            />
-            <Button
-              type='submit'
-              variant='contained'
-              color='primary'
-              onClick={handleClick}
-              fullWidth
+    <>
+      <Spacer />
+      <Grid.Container gap={2} justify='center'>
+        <Grid>
+          <Container>
+            <Card
+              style={{
+                padding: '2rem',
+                paddingLeft: '5rem',
+                paddingRight: '5rem',
+              }}
             >
-              Submit
-            </Button>
-          </form>
-          {submissionStatus && <p>{submissionStatus}</p>}
-          {submittedReimbursement && (
-            <div>
-              <h3>Submitted Reimbursement:</h3>
-              <p>Ticket Number: {submittedReimbursement.id}</p>
-              <p>Description: {submittedReimbursement.description}</p>
-              <p>Status: {submittedReimbursement.status}</p>
-              <p>Expense Amount: {submittedReimbursement.expenseAmount}</p>
-            </div>
-          )}
-        </div>
-      </Paper>
-    </Container>
+              <Card.Body>
+                <Text
+                  h1
+                  size={30}
+                  css={{
+                    textAlign: 'center',
+                    textGradient: '45deg, $yellow600 -20%, $red600 100%',
+                  }}
+                  weight='bold'
+                >
+                  Submit A Ticket
+                </Text>
+                <Spacer y={2} />
+                <Input
+                  auto
+                  bordered
+                  color='secondary'
+                  labelPlaceholder='Description'
+                  placeholder='Enter Description'
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  required
+                />
+                <Spacer y={2} />
+                <Input
+                  auto
+                  bordered
+                  initialValue='123'
+                  color='secondary'
+                  labelPlaceholder='Enter Amount'
+                  placeholder='Enter Expense Amount'
+                  type='number'
+                  value={expenseAmount}
+                  onChange={(e) => setExpenseAmount(e.target.value)}
+                  required
+                />
+                <Spacer y={2} />
+                <Button
+                  type='submit'
+                  variant='contained'
+                  color='primary'
+                  onClick={handleClick}
+                  fullWidth
+                >
+                  Submit
+                </Button>
+              </Card.Body>
+              {submissionStatus && (
+                <Text
+                  size={25}
+                  color='primary'
+                  css={{
+                    textAlign: 'center',
+                  }}
+                >
+                  {submissionStatus}
+                </Text>
+              )}
+              {submittedReimbursement && (
+                <div>
+                  <Text
+                    css={{
+                      textAlign: 'center',
+                    }}
+                  >
+                    Ticket Number: {submittedReimbursement.id}
+                  </Text>
+                  <Text
+                    css={{
+                      textAlign: 'center',
+                    }}
+                  >
+                    Description: {submittedReimbursement.description}
+                  </Text>
+                  <Text
+                    css={{
+                      textAlign: 'center',
+                    }}
+                  >
+                    Status: {submittedReimbursement.status}
+                  </Text>
+                  <Text
+                    css={{
+                      textAlign: 'center',
+                    }}
+                  >
+                    Expense Amount: {submittedReimbursement.expenseAmount}
+                  </Text>
+                </div>
+              )}
+            </Card>
+          </Container>
+        </Grid>
+      </Grid.Container>
+    </>
   );
 }
